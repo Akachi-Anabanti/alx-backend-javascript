1b48004413c97c08e993d1133b2f07e044b28d32
Akachi Anabanti <akachi.anabanti@gmail.com>
2024-06-08 14:27:21 +0100

    eslint -fix

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -2,10 +2,10 @@ import { uploadPhoto, createUser } from './utils';
 
 export default async function handleProfileSignup() {
   try {
-    const {firstName, lastName} = await createUser();
-    const {body} = await uploadPhoto();
+    const { firstName, lastName } = await createUser();
+    const { body } = await uploadPhoto();
     console.log(body, firstName, lastName);
-  } catch {
-    return new Error('Signup system offline');
+  } catch (e) {
+    console.log(new Error('Signup system offline'));
   }
 }
