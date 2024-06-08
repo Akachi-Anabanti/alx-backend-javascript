eb2d665a0b19631fad8f45f16ed027e0aa3dcba0
Akachi Anabanti <akachi.anabanti@gmail.com>
2024-06-08 15:30:36 +0100

    final promise all settled

---
+++ 0x01-ES6_promise/6-final-user.js
@@ -0,0 +1,7 @@
+import uploadPhoto from './5-photo-reject';
+import signUpUser from './4-user-promise';
+
+export default function handleProfileSignup(firstName, lastName, fileName) {
+  return Promise.allSettled([signUpUser(firstName, lastName), uploadPhoto(fileName)])
+    .then((res) => res);
+}
