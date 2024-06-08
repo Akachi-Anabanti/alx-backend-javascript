4b41026ca983646d140bcbc3398d410a2d1fe6ba
Akachi Anabanti <akachi.anabanti@gmail.com>
2024-06-08 00:08:30 +0000

    function missing return statement fix

--- 0x01-ES6_promise/2-then.js
+++ 0x01-ES6_promise/2-then.js
@@ -1,10 +1,8 @@
 export default function handleResponseFromAPI(promise) {
-  promise
+  return promise
   // eslint-disable-next-line no-unused-vars
     .then((res) => ({ status: 200, body: 'success' }))
   // eslint-disable-next-line no-unused-vars
     .catch((e) => new Error())
-    .finally(
-      console.log('Got a response from the API'),
-    );
+    .finally(() => { console.log('Got a response from the API'); });
 }
