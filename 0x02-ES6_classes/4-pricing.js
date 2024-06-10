6b9b4e9597fbdba579c23d9262d88fa95d1e2055
Akachi Anabanti <akachi.anabanti@gmail.com>
2024-06-10 20:35:03 +0000

    more classes and static method

---
+++ 0x02-ES6_classes/4-pricing.js
@@ -0,0 +1,41 @@
+// eslint-disable-next-line no-unused-vars
+import Currency from './3-currency';
+
+export default class Pricing {
+  constructor(amount, currency) {
+    this._amount = amount;
+    this._currency = currency;
+  }
+
+  get amount() {
+    return this._amount;
+  }
+
+  set amount(value) {
+    if (typeof (value) === 'number') {
+      this._amount = value;
+    } else {
+      throw new TypeError('Amount must be a number');
+    }
+  }
+
+  get currency() {
+    return this._currency;
+  }
+
+  set currency(value) {
+    if (typeof (value) === 'string') {
+      this._currency = value;
+    } else {
+      throw new TypeError('Currency must be a string');
+    }
+  }
+
+  displayFullPrice() {
+    return `${this._amount} ${this._currency.displayFullCurrency()}`;
+  }
+
+  static convertPrice(amount, conversionRate) {
+    return amount * conversionRate;
+  }
+}
