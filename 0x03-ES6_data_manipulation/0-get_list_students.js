402f7dbc14f9ac84bd9c659a19252c6b3d2ebc70
Akachi Anabanti <akachi.anabanti@gmail.com>
2024-06-13 11:12:18 +0100

    function to return list

---
+++ 0x03-ES6_data_manipulation/0-get_list_students.js
@@ -0,0 +1,7 @@
+export default function getListStudents() {
+  return [
+    { id: 1, location: 'San Francisco', firstName: 'Guillaume' },
+    { id: 2, location: 'Columbia', firstName: 'James' },
+    { id: 5, location: 'San Francisco', firstName: 'Serena' },
+  ];
+}
