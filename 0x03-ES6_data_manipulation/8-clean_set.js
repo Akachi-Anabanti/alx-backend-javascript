a7d74f9b96ac24b4f67f9cb6e27e84b2135c8768
Akachi Anabanti <akachi.anabanti@gmail.com>
2024-06-15 16:39:36 +0100

    fix: string check

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,6 +1,6 @@
 export default function cleanset(set, startString) {
   if (!startString) return '';
-  return Array.from(set).filter((el) => el.startsWith(startString))
+  return Array.from(set).filter((el) => el && el.startsWith(startString))
     .map((word) => word.slice(startString.length))
     .join('-');
 }
