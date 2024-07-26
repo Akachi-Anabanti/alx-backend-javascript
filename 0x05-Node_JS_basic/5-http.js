8184c26bc0c829c9136c2656e5207ada4ecbe0d5
Akachi Anabanti <akachi.anabanti@gmail.com>
2024-07-26 21:03:59 +0000

    improved http server

--- 0x05-Node_JS_basic/5-http.js
+++ 0x05-Node_JS_basic/5-http.js
@@ -1,16 +1,54 @@
 // a more complex HTTP server using
 //
 const http = require('http');
-const countStudents = require('./3-read_file_async');
+const fs = require('fs');
 
 const PORT = 1245;
 const HOST = 'localhost';
 
-
 const app = http.createServer();
 
-const DB = process.argv.length > 2 ? process.argv[2]: 'database.csv';
+const DB = process.argv.length > 2 ? process.argv[2] : '';
 
+const countStudents = (filePath) => new Promise((resolve, reject) => {
+  if (!filePath) {
+    reject(new Error('Cannot load the database'));
+  }
+  if (filePath) {
+    fs.readFile(filePath, (err, data) => {
+      if (err) {
+        reject(new Error('Cannot load the database'));
+      }
+
+      if (data) {
+        const reportParts = [];
+
+        const lines = data.toString('utf-8').trim().split('\n');
+
+        const students = lines.slice(1);
+
+        reportParts.push(`Number of students: ${students.length}`);
+
+        const fields = {};
+
+        students.forEach((student) => {
+          const [firstName, , , field] = student.split(',');
+          if (!fields[field]) {
+            fields[field] = { count: 0, list: [] };
+          }
+          fields[field].count += 1;
+          fields[field].list.push(firstName);
+        });
+
+        Object.entries(fields).forEach(([field, { count, list }]) => {
+          reportParts.push(`Number of students in ${field}: ${count}. List: ${list.join(', ')}`);
+        });
+
+        resolve(reportParts.join('\n'));
+      }
+    });
+  }
+});
 
 const SERVER_ROUTE_HANDLERS = [
   {
@@ -38,22 +76,20 @@ const SERVER_ROUTE_HANDLERS = [
           res.statusCode = 200;
           res.write(Buffer.from(responseText));
         })
-      .catch((err) => {
-        responseParts.push(err instanceof Error ? err.message: err.toString());
-        const responseText = responseParts.join('\n');
-        res.setHeader('Content-Type', 'text/plain');
-        res.setHeader('Content-Length', responseText.length);
-        res.statusCode = 200;
-        res.write(Buffer.from(responseText));
-      });
-
+        .catch((err) => {
+          responseParts.push(err instanceof Error ? err.message : err.toString());
+          const responseText = responseParts.join('\n');
+          res.setHeader('Content-Type', 'text/plain');
+          res.setHeader('Content-Length', responseText.length);
+          res.statusCode = 200;
+          res.write(Buffer.from(responseText));
+        });
     },
   },
-]
-
+];
 
 app.on('request', (req, res) => {
-  for (let routeHandler of SERVER_ROUTE_HANDLERS) {
+  for (const routeHandler of SERVER_ROUTE_HANDLERS) {
     if (routeHandler.route === req.url) {
       routeHandler.handler(req, res);
       return;
